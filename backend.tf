4f1ef5c816fa918cff10b0e9f53714aab5fdc72c
ViktorSvertoka <victor.svertoka@gmail.com>
2025-08-08 11:35:55 +0300

    Update files

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "victorsvertoka-terraform-state-lesson-5"
+    bucket = "victorsvertoka-terraform-state-lesson-7"
     key            = "lesson-7/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
